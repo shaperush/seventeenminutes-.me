50ef7e159e75f155230b6e6d6c0984dc34c6e52f
shaperush <30592293+shaperush@users.noreply.github.com>
2024-02-04 00:27:55 +0200

    update title

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import videoBg from '../assets/myvideo.mov'
 
+const TITLE = 'My Page Title';
 
 const Main = () => {
   return (
     <div className='main'>
+      <Helmet>
+          <title>{ TITLE }</title>
+      </Helmet>
         <div className="overlay"></div>
         <video src={videoBg} autoPlay loop muted />
         <div className="content">
