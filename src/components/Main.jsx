b5f9b975f8774e232308389b94e25b59c78a8190
shaperush <30592293+shaperush@users.noreply.github.com>
2024-02-04 01:13:54 +0200

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,6 +7,8 @@ const Main = () => {
         <div className="overlay"></div>
         <video src={videoBg} autoPlay loop muted />
         <div className="content">
+          <h1>Welcome</h1>
+            <p>To my site.</p>
         </div>
     </div>
   )
